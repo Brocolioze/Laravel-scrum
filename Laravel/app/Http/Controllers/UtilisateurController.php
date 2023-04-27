fd00f83b561c48cdd8ed35ac5b74272b6539add1
Boaz <49524886+Brocolioze@users.noreply.github.com>
2023-04-27 10:22:21 -0400

    indentation et verif du code

--- Laravel/app/Http/Controllers/UtilisateurController.php
+++ Laravel/app/Http/Controllers/UtilisateurController.php
@@ -13,85 +13,63 @@ use App\User;
 class UtilisateurController extends Controller
 {
 
-
-// Montre la liste des utilisateurs
+    // Affiche la liste des utilisateurs dans la base de donnees
     public function index()
     {
+
         $utilisateurs = Utilisateur::latest()->paginate(5);
     
-        return view('utilisateurs.index',compact('utilisateurs'));
+            return view('utilisateurs.index',compact('utilisateurs'));
     }
 
 
-    
-// Montre le formulaire de creation des utilisateurs
+    // Affiche le formulaire de creation des utilisateurs
     public function create()
     {
         return view('utilisateurs.create');
     }
 
 
+    // Fonction Store pour la verification des donnees
     public function store(Request $request)
-        {
+    {
+        $validator = Validator::make($request->all(), 
 
-     
-
-      $validator = Validator::make($request->all(), [
+      [
         'nom' => 'required|min:2',
         'prenom' => 'required|min:2',
         'matricule' => 'required|min:7',
         'email' => 'required|email|unique:users,email,'.$utilisateurs->id,
         'mot_de_passe' => 'required|min:8'
-
-     ] );
+      ]);
    
-        if($validator->fails()){
-
-                 return redirect('inscription')->withErrors($validator)->withInput();
-
-            }
-
-
-        /*
-        $request->validate([
-           
-            'nom' => 'required',
-            'prenom' => 'required',
-            'matricule' => 'required',
-            'email' => 'required',
-            'mot_de_passe' => 'required'
-        ]);
-    
-        utilisateurs::create($request->all());
-     
-        return redirect()->route('utilisateurs.index')
-                        ->with('success','Product created successfully.');
-
-                        */
+        if($validator->fails())
+        {
+            return redirect('inscription')->withErrors($validator)->withInput();
+        }
     }
 
-
-    // Montre les informations  des utilisateurs
+    // Affiche les informations  des utilisateurs
     public function show(Utilisateur $utilisateur)
     {
         return view('utilisateurs.show',compact('utilisateur'));
     } 
 
-// editions des utilisateurs
+    // editions des utilisateurs
     public function edit(Utilisateur $utilisateur)
     {
         return view('utilisateurs.edit',compact('utilisateur'));
     }
 
-    // Maj des utilisateurs
+    // Mise a jour des donnees des utilisateurs
     public function update(Request $request, Utilisateur $utilisateur)
     {
         $request->validate([
-        'nom' => 'required|min:3',
-        'prenom' => 'required|min:3',
-        'matricule' => 'required|min:7',
-        'email' => ['required', 'unique:users', 'email', 'unique:users,email' . $user->id],
-        'mot_de_passe' => 'required|min:8'
+            'nom' => 'required|min:3',
+            'prenom' => 'required|min:3',
+            'matricule' => 'required|min:7',
+            'email' => ['required', 'unique:users', 'email', 'unique:users,email' . $user->id],
+            'mot_de_passe' => 'required|min:8'
         ]);
     
         $utilisateur->update($request->all());
@@ -100,31 +78,26 @@ class UtilisateurController extends Controller
                         ->with('success','Product updated successfully');
     }
 
-    //suppression  des utilisateurs
+    //Suppression des utilisateurs
     public function destroy(Utilisateur $utilisateur)
     {
-
   
         $utilisateur::find($id)->delete();
         $utilisateur->delete();
-      
-    
-        return redirect()->route('utilisateurs.index');
 
-        
+         return redirect()->route('utilisateurs.index');
     }
 
 
-    public function form_register(){
-
+    public function form_register()
+    {
         $title = "Page d'inscription";
 
-        return view('inscription',compact('title'));
+             return view('inscription',compact('title'));
     }
 
-    public function traitement_register(Request $request){
-
-        
+    public function traitement_register(Request $request)
+    {
 
         $request->validate([
 
@@ -133,36 +106,28 @@ class UtilisateurController extends Controller
             'matricule' => 'required|min:7',
             'email' => 'required',
             'mot_de_passe' => 'required|min:8'
+
         ]);
-
-
-        
 
         $verif_email = Utilisateur::where('email', $request->email)->first();
 
-        if($verif_email){
+        if($verif_email) {
 
             $maileroor =  "email deja existant";
-            return redirect('inscription')->withErrors($maileroor)->withInput();
+                 return redirect('inscription')->withErrors($maileroor)->withInput();
 
-        }else{
+         } else {
 
             $utilisateur = new Utilisateur();
-
             $utilisateur -> nom = $request->input('nom');
             $utilisateur -> prenom = $request->input('prenom');
             $utilisateur -> matricule = $request->input('matricule');
             $utilisateur -> mot_de_passe = bcrypt($request->input('mot_de_passe'));
             $utilisateur -> email = $request->input('email');
             $utilisateur -> save();
-    
-            return redirect('/connection')->with('status','Votre compte a ete cree. ');
-    
-    
 
-
+                return redirect('/connection')->with('status','Votre compte a ete cree. ');
         }
-
        
     }
 
@@ -174,15 +139,10 @@ class UtilisateurController extends Controller
         return view('connection',compact('title'));
     }
 
-
-
-
-    public function traitement_connection (Request $request){
+    public function traitement_connection (Request $request)
+    {
   
-
         $title = "Page de connection";
-
-        
 
         $request->validate([
           
@@ -214,22 +174,12 @@ class UtilisateurController extends Controller
        
 }
 
-//deconnexion
-public function deconnexion()
-{
-    auth()->logout();
-
-    $request->session()->invalidate();
- 
-
-    return redirect('/');
-}
-
-
-
-
-
-
-
+        //deconnexion
+        public function deconnexion()
+        {
+           auth()->logout();
+           $request->session()->invalidate();
+           return redirect('/');
+        }
 
 }
